f0e731acddaa4614f192cca6058f73b54183e17a
Carlos <carlos_alberto_lira@hotmail.com>
2021-05-01 23:17:46 -0500

    Se corrigio el problema del button al desplegarse la navegacion

--- App.js
+++ App.js
@@ -2,15 +2,16 @@ addEventListener("DOMContentLoaded", () => {
   const btn_menu = document.querySelector(".btn_menu");
   if (btn_menu) {
     btn_menu.addEventListener("click", () => {
-      const menu_items = document.querySelector(".menu_items");
-      menu_items.classList.toggle("mostrar");
-      btn_menu.classList.toggle("ocultar");
-      const logo = document.querySelector(".logo");
-      logo.classList.toggle("ocultar_logo");
-      const cerrar = document.querySelector(".btn_cerrar");
-      cerrar.classList.toggle("mostrarCerrar");
-      // const menu = document.querySelector(".btn");
-      // menu.classList.toggle("cerrar");
+      const menu_items = document.querySelector(".menu");
+      menu_items.classList.toggle("active");
+    });
+  }
+
+  const btn_cerrar = document.querySelector(".btn_cerrar");
+  if (btn_cerrar) {
+    btn_cerrar.addEventListener("click", () => {
+      const menu_items = document.querySelector(".menu");
+      menu_items.classList.remove("active");
     });
   }
 });
